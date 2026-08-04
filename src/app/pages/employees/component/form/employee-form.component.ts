f800415e057924e82b1b8bc665bf0330894982e6
Madoona-eng <madonnaessam2002@gmail.com>
2026-08-04 11:43:42 +0300

    donee btn back

--- src/app/pages/employees/component/form/employee-form.component.ts
+++ src/app/pages/employees/component/form/employee-form.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { Employee } from '../../model/models';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.css']
 })
@@ -18,6 +19,7 @@ export class EmployeeFormComponent {
   @Input() errorMessage = '';
   @Input() departmentOptions: { id: number; name: string }[] = [];
   @Input() hideNotes = false;
+  @Input() showPageShell = false;
 
   @Output() submitForm = new EventEmitter<void>();
   @Output() cancel = new EventEmitter<void>();
